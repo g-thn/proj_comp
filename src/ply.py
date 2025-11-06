f2af3f2afb7aabe56abded699d1e17d677e36fd9
g-thn <gillestahan@gmail.com>
2025-11-06 18:49:05 +0100

    Added Tsai-Hill criterion

--- src/ply.py
+++ src/ply.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Ply_comp:
-    def __init__(self,e1, e2, g12, nu12,z_top,z_bot,theta):
+class Ply:
+    def __init__(self,e1, e2, g12, nu12,z_top,z_bot,theta, x = 0., y = 0., s = 0.):
         self.e1 = e1
         self.e2 = e2
         self.g12 = g12
@@ -12,13 +12,17 @@ class Ply_comp:
         self.z_bot = z_bot
         self.thickness = z_top - z_bot
         self.theta = theta
-        self.invmatrot = self.compute_invmatrot()
-        self.q_local = self.q_loc()
-        self.q_global = self.q_glob()
-        self.a = self.a_loc()
-        self.b = self.b_loc()
-        self.d = self.d_loc()
-        self.abd = self.abd_loc()
+        self.x = x
+        self.y = y
+        self.s = s
+        self.compute_invmatrot()
+        self.matrot = np.linalg.inv(self.invmatrot)
+        self.q_loc()
+        self.q_glob()
+        self.a_loc()
+        self.b_loc()
+        self.d_loc()
+        self.abd_loc()
         self.eps0 = np.array([[0],
                               [0],
                               [0]])
@@ -30,25 +34,33 @@ class Ply_comp:
     def compute_invmatrot(self):
         c = np.cos(self.theta)
         s = np.sin(self.theta)
-        return np.array([[c**2, s**2,      -2*s*c],
-                         [s**2, c**2,       2*s*c],
-                         [ c*s, -c*s, c**2 - s**2]])
+        self.invmatrot = np.array([[c**2, s**2,      -2*s*c],
+                                   [s**2, c**2,       2*s*c],
+                                   [ c*s, -c*s, c**2 - s**2]])
+        self.matrot = np.linalg.inv(self.invmatrot)
+        return self.invmatrot
 
     def q_loc(self):
-        return np.array([[self.e1/(1 - self.nu12*self.nu21)        , self.nu21*self.e1/(1 - self.nu12*self.nu21), 0       ],
-                         [self.nu12*self.e2/(1 - self.nu12*self.nu21), self.e2/(1 - self.nu12*self.nu21)          , 0       ],
-                         [0                                        , 0                                          , self.g12]])
+        self.q_local = np.array([[self.e1/(1 - self.nu12*self.nu21)        , self.nu21*self.e1/(1 - self.nu12*self.nu21), 0       ],
+                                 [self.nu12*self.e2/(1 - self.nu12*self.nu21), self.e2/(1 - self.nu12*self.nu21)          , 0       ],
+                                 [0                                        , 0                                          , self.g12]])
+        return self.q_local
+     
     def q_glob(self):
-        return np.dot(np.dot(self.invmatrot, self.q_local), self.invmatrot.T)
+        self.q_global = np.dot(np.dot(self.invmatrot, self.q_local), self.invmatrot.T)
+        return self.q_global
     
     def a_loc(self):
-        return self.q_global*(self.z_top - self.z_bot)
+        self.a = self.q_global*(self.z_top - self.z_bot)
+        return self.a
     
     def b_loc(self):
-        return 0.5*self.q_global*(self.z_top**2 - self.z_bot**2)
+        self.b = 0.5*self.q_global*(self.z_top**2 - self.z_bot**2)
+        return self.b
     
     def d_loc(self):
-        return self.q_global*(self.z_top**3 - self.z_bot**3)/3
+        self.d = self.q_global*(self.z_top**3 - self.z_bot**3)/3
+        return self.d
     
     def abd_loc(self):
         a = self.a_loc()
@@ -56,6 +68,7 @@ class Ply_comp:
         d = self.d_loc()
         abd = np.block([[a, b],
                         [b, d]])
+        self.abd = abd
         return abd
     
     def set_eps_phi(self, eps, phi):
@@ -69,54 +82,142 @@ class Ply_comp:
         stress = np.dot(self.q_global, self.strain(z))
         return stress
     
-    def set_param_TS(self, X, Y, S):
-        self.X = X
-        self.Y = Y
-        self.S = S
+    def set_param_Ts(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
 
     def tsai_hill_crit(self):
-        sig_glob_top = self.stress(self.z_top)
-        sig_glob_bot = self.stress(self.z_bot)
-        sig_loc_top = np.dot(self.invmatrot, sig_glob_top)
-        sig_loc_bot = np.dot(self.invmatrot, sig_glob_bot)
-        pass
-        
-        
+        crit_values = []
+        for z in [self.z_top, self.z_bot]:
+            sig_glob = self.stress(z)
+            sig_loc = np.dot(self.matrot, sig_glob)
+            sig1 = sig_loc[0,0]
+            sig2 = sig_loc[1,0]
+            tau12 = sig_loc[2,0]
+            crit = (sig1/self.x)**2 - (sig1*sig2)/(self.x**2) + (sig2/self.y)**2 + (tau12/self.s)**2
+            crit_values.append(crit)
+        self.tscrit =  np.max(crit_values)
+        return self.tscrit
 
+class Laminate:
+    def __init__(self, ply_list):
+        self.ply_list = ply_list
+        self.abd_global()
+        self.abd_inv = np.linalg.inv(self.abd)
+        self.load = np.zeros((6,1))
+        self.deformation = np.zeros((6,1))
+        self.eps0 = np.zeros((3,1))
+        self.phi = np.zeros((3,1))
+        self.force = np.zeros((3,1))
+        self.moment = np.zeros((3,1))
+        self.comp_thickness()
+        self.z_top = .5*self.thickness
+        self.z_bot = -.5*self.thickness
+        self.tscrit = 0.
+
+    
+    def comp_thickness(self):
+        self.thickness = 0.
+        for ply in self.ply_list:
+            self.thickness += ply.thickness
+
+    def set_load(self, force, moment):
+        self.force = force
+        self.moment = moment
+        self.load = np.vstack((force, moment))
+    
+    def set_deformation(self, eps_top, eps_bot):
+        self.eps0 = 0.5*(eps_top + eps_bot)
+        self.phi = (eps_top - eps_bot)/self.thickness
+        self.deformation = np.vstack((self.eps0, self.phi))
+        self.update_plies()
+        
+    def update_plies(self):
+        for ply in self.ply_list:
+            ply.set_eps_phi(self.eps0, self.phi)
+
+    def abd_global(self):
+        a_tot = np.zeros((3,3))
+        b_tot = np.zeros((3,3))
+        d_tot = np.zeros((3,3))
+        for ply in self.ply_list:
+            a_tot += ply.a_loc()
+            b_tot += ply.b_loc()
+            d_tot += ply.d_loc()
+        abd_tot = np.block([[a_tot, b_tot],
+                            [b_tot, d_tot]])
+        self.abd = abd_tot
+        return abd_tot
+    
+    def comp_deformation(self):
+        self.deformation = np.dot(self.abd_inv, self.load)
+        self.eps0 = self.deformation[0:3,:]
+        self.phi = self.deformation[3:6,:]
+        self.update_plies()
+        return self.deformation
+    
+    def comp_load(self):
+        self.load = np.dot(self.abd, self.deformation)
+        self.force = self.load[0:3,:]
+        self.moment = self.load[3:6,:]
+        return self.load
+
+    def update_tsai_hill(self):
+        tscrit_list = []
+        for ply in self.ply_list:
+            ply.set_eps_phi(self.eps0, self.phi)
+            tscrit = ply.tsai_hill_crit()
+            tscrit_list.append(tscrit)
+        self.tscrit = np.max(tscrit_list)
+
+    def plot_laminate(self):
+        pass
+    
+    def plot_stress_distribution(self, num_points=100):
+        pass
+    
 if __name__ == "__main__":
-    e1 = 25e9
-    e2 = 5e9
+    
+    # Param ply
+    e1 = 77e9
+    e2 = 77e9
     g12 = 10e9
-    nu12 = 0.4
-    theta1 = 30.*np.pi/180.
+    nu12 = 0.
+    xc = 666e6
+    yc = 666e6
+    sc = 94e6
+    # Param foam
+    ef = 27e6
+    nuf = 0.1
+    gf = ef/(2*(1+nuf))
+    xf = .4e6
+    yf = .4e6
+    sf = .15e6
+    theta1 = 0.*np.pi/180.
     theta2 = 0.*np.pi/180.
-    theta3 = -30.*np.pi/180.
-    z1 = 6e-3
+    theta3 = 0.*np.pi/180.
+    z1 = 4-3
     z2 = 3e-3
     z3 = -3e-3
-    z4 = -6e-3
-    ply1 = Ply_comp(e1, e2, g12, nu12, z1, z2, theta1)
-    ply2 = Ply_comp(e1, e2, g12, nu12, z2, z3, theta2)
-    ply3 = Ply_comp(e1, e2, g12, nu12, z3, z4, theta3)
-    a_tot = ply1.a_loc() + ply2.a_loc() + ply3.a_loc()
-    b_tot = ply1.b_loc() + ply2.b_loc() + ply3.b_loc()
-    d_tot = ply1.d_loc() + ply2.d_loc() + ply3.d_loc()
-    print("A matrix:")
-    print(a_tot)
-    print("B matrix:")
-    print(b_tot)
-    print("D matrix:")
-    print(d_tot)
-    eps0 = np.array([[0.001],
-                     [0.002],
-                     [0.005]])
-    phi = np.array([[0.03],
-                    [0.01],
-                    [0.004]])
-    ply1.set_eps_phi(eps0, phi)
-    strain_fun = ply1.strain()
-    z = np.linspace(ply1.z_bot, ply1.z_top, )
-    print("Strain through the thickness of ply 1:")
-    print(strain_fun(z))
-    plt.plot(z, strain_fun(z)[0,:], label='epsilon_x')
-    plt.show()
+    z4 = -4e-3
+    ply1 = Ply(e1, e2, g12, nu12, z1, z2, theta1, x=xc, y=yc, s=sc)
+    ply2 = Ply(ef, ef, gf, nuf, z2, z3, theta2, x=xf, y=yf, s=sf)
+    ply3 = Ply(e1, e2, g12, nu12, z3, z4, theta3, x=xc, y=yc, s=sc)
+    ply_list = [ply1, ply2, ply3]
+    laminate = Laminate(ply_list)
+    laminate.abd_global()
+    eps_top = 1e-3*np.array([[1.75],
+                             [-6.37],
+                             [0.0]])
+    eps_bot = 1e-3*np.array([[-2.46],
+                             [6.6],
+                             [0.0]])
+    laminate.set_deformation(eps_top, eps_bot)
+    print("Laminate eps0:", laminate.eps0.flatten())
+    print("Laminate phi:", laminate.phi.flatten())
+    laminate.update_tsai_hill()
+    print("Tsai-Hill criterion:", laminate.ply_list[0].tscrit, laminate.ply_list[1].tscrit, laminate.ply_list[2].tscrit)
+    print("Max Tsai-Hill criterion;", laminate.tscrit)
+    laminate.plot_laminate()
+    laminate.plot_stress_distribution()
